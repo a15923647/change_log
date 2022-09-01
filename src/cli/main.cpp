28b4d7a14cc276bd5c4eb80ba9fcfe735c7f20b2
jzwang <a15923647@gmail.com>
2022-09-01 21:17:06 +0800

    sqlite cli

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string.h>
 #include <regex>
 #include <chrono>
 #include <unordered_map>
 #include <filesystem>
-#include "vector_storage.h"
+#include <sqlite3.h>
+#include <unordered_map>
+#include "sqlite_util.h"
+#include "sqlite_storage.h"
 #include "event.h"
 #include "patch.h"
 #include "file.h"
@@ -13,126 +17,116 @@
 
 using namespace std;
 
-void read_from_dump(ifstream& fin, unordered_map<string, vector<Node>>& nodem, regex& pat) {
-  size_t size; fin >> size; fin.get(); //eat endl at ../daemon/main.cpp:23
-  while (size--) {
-    Node node(0);
-    fin >> node; fin.get(); //eat endl at ../daemon/main.cpp:25
-    if (regex_match(node.path, pat)) {
-      if (!nodem.count(node.path)) {
-        nodem[node.path] = vector<Node>();
-      }
-      nodem[node.path].push_back(node);
+void list(string db_loc, regex& pat) {
+  string sql = "SELECT * FROM history_meta;";
+  vector<history_meta_entry> res;
+  send_query(db_loc, sql, history_meta_handler, (void *)&res);
+  unordered_map<string, vector<history_meta_entry>> path_history;
+
+  for (auto& e : res) {
+    if (!regex_match(e.path, pat)) continue;
+    if (!path_history.count(e.path)) {
+      path_history[e.path] = vector<history_meta_entry>();
+    }
+    path_history[e.path].push_back(e);
+  }
+  for (auto& [k, v] : path_history) {
+    cout << k << " " << v.size() << " time modify\n";
+  }
+}
+
+void ll(string db_loc, regex& pat) {
+  string sql = "SELECT * FROM history_meta;";
+  vector<history_meta_entry> res;
+  send_query(db_loc, sql, history_meta_handler, (void *)&res);
+  unordered_map<string, vector<history_meta_entry>> path_history;
+
+  for (auto& e : res) {
+    if (!regex_match(e.path, pat)) continue;
+    if (!path_history.count(e.path)) {
+      path_history[e.path] = vector<history_meta_entry>();
+    }
+    path_history[e.path].push_back(e);
+  }
+
+  for (auto& [k, v] : path_history) {
+    cout << k << endl;
+    for (auto& e : v) {
+      cout << "  " + evTypeStringify[e.ev_type] + " at " << e.trigger_time << endl;
     }
   }
 }
 
-void ll(ifstream& fin, regex& pat) {
-  unordered_map<string, vector<Node>> nodem;
-  read_from_dump(fin, nodem, pat);
-  for (auto& [k, v] : nodem) {
-    cout << k << endl;
-    reverse(v.begin(), v.end());
-    cout << "  time length\n";
-    for (auto& node: v)
-      cout << "  " << node.trigger_time << " " << node.op_list.content_length() << endl;
-  }
+void get_cur_content(string db_loc, string& path, string& cur_content) {
+  string sql = "SELECT content FROM file_content WHERE path = '" + double_single_quote(path) + "';";
+  send_query(db_loc, sql, get_first_text, (void *)&cur_content);
 }
 
-void list(ifstream& fin, regex& pat) {
-  unordered_map<string, vector<Node>> nodem;
-  read_from_dump(fin, nodem, pat);
-  for (auto& [k, v] : nodem) {
-    size_t total_op_cnt = 0;
-    for (auto& node : v)
-      total_op_cnt += node.op_list.content_length();
-    cout << k << " " << total_op_cnt << " edits\n";
-  }
+void str2opl(string& s, OperationList& opl) {
+  stringstream ss;
+  ss << s;
+  ss >> opl;
 }
 
-void back(ifstream& fin, regex& pat, time_t til) {
-  puts("back");
+void back(string db_loc, regex& pat, time_t til) {
   auto patch = Patch();
-  unordered_map<string, vector<Node>> nodem;
-  read_from_dump(fin, nodem, pat);
+  string sql = "SELECT * FROM history_meta JOIN history_opl ON history_meta.id = history_opl.id WHERE trigger_time >= " + to_string(max((time_t)0, til)) +";";
+  vector<history_entry> qres;
+  send_query(db_loc, sql, history_handler, (void *)&qres);
+
+  unordered_map<string, vector<history_entry>> path_history;
+  for (auto& e : qres) {
+    if (!regex_match(e.path, pat)) continue;
+    if (!path_history.count(e.path)) {
+      path_history[e.path] = vector<history_entry>();
+    }
+    path_history[e.path].push_back(e);
+  }
   string res;
-  for (auto& [k, v] : nodem) {
+  for (auto& [k, v] : path_history) {
     string cur_content;
     string path_str(k);
     std::filesystem::path path(path_str);
-    read_whole_file(path_str, cur_content);//file content may not match the operation context
+    get_cur_content(db_loc, path_str, cur_content);
     reverse(v.begin(), v.end());//patch back from back
-    //future work: check modify time or hash
-    /*
-    auto ftime = fs::last_write_time(path);
-    time_t mtime = decltype(ftime)::clock::to_time_t(ftime);
-    if (abs(mtime - v[0].trigger_time) > 0.5) {
-      cerr << "mtime mismatch!\n";
-      exit(3);
-    }
-    */
-    for(auto& node : v) {
-      if (node.trigger_time < til) break;
-      vector<edit_operation> *eop = node.op_list.diff_list();
+    history_entry& last = v[0];
+    int patch_times = (til < 0) ? -til : 0x7fffffff;
+    for(auto& ent : v) {
+      if (patch_times-- < 0) break;
+      OperationList op_list;
+      str2opl(ent.opl_dump, op_list);
+      vector<edit_operation> *eop = op_list.diff_list();
       patch.patchBack(cur_content, *eop, res);
       delete eop;
-      puts("----------------------------------------");
-      cout << res;
       cur_content = res;
       res = "";
+      last = ent;
     }
-    cout << "previous version: \n";
+    cout << "recover " + k + " to version back to " << last.trigger_time << endl;
     cout << cur_content;
     //DO SOMETHING HERE!
   }
 }
 
-ifstream get_if(char *path) {
-  ifstream f(path);
-  if (!f.good()) {
-    cerr << "file is not good\n";
-    exit(2);
-  }
-  return f;
-}
-
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    cerr << argv[0] << " command\n";
+  if (argc < 3) {
+    cerr << argv[0] << " command db_path [path_pattern]\n";
+    cerr << "command:\nll\nlist\nback\n";
     exit(1);
   }
-  regex all(".*");
+  string db_loc(argv[2]);
+  regex pat(".*");
+  if (argc > 3)
+    pat = regex(argv[3]);
   if (strcmp(argv[1], "back") == 0) {
-    if (argc < 3) {
-      cout << argv[0] << " back patch_file [path_pattern]\n";
-      return 1;
-    }
-    ifstream fin = get_if(argv[2]);
-
-    regex pat(".*");
-    if (argc > 3)
-      pat = regex(argv[3]);
-
     time_t til = 0;
     if (argc > 4)
       til = stoi(argv[4]);
-
-    back(fin, pat, til);
-    fin.close();
-  } else if (strcmp(argv[1], "list")) {
-    if (argc < 3) {
-      cout << argv[0] << " list patch_file [path_pattern]\n";
-      return 1;
-    }
-    ifstream fin = get_if(argv[2]);
-    list(fin, all);
-  } else if (strcmp(argv[1], "ll")) {
-    if (argc < 3) {
-      cout << argv[0] << " ll patch_file [path_pattern]\n";
-      return 1;
-    }
-    ifstream fin = get_if(argv[2]);
-    ll(fin, all);
+    back(db_loc, pat, til);
+  } else if (strcmp(argv[1], "list") == 0) {
+    list(db_loc, pat);
+  } else if (strcmp(argv[1], "ll") == 0) {
+    ll(db_loc, pat);
   }
   return 0;
 }
--- src/include/event.h
+++ src/include/event.h
@@ -4,6 +4,9 @@
 #include <time.h>
 enum EventType {MODIFY = 0, CREATE = 1, DELETE = 2};
 const EventType num2ev_type[3] = {EventType::MODIFY, EventType::CREATE, EventType::DELETE};
+const std::string evTypeStringify[3] = {
+  "modify", "create", "delete"
+};
 struct Event {
   EventType ev_type;
   time_t trigger_time;
