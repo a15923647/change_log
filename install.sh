cfffab024656bf3ec2d0088c9ffb37e3dab31e93
jzwang <a15923647@gmail.com>
2022-09-01 22:34:27 +0800

    fix service file

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -x
 ls cl-cli cl-daemon 2> /dev/null || ./compile.sh
-mv cl-daemon /usr/bin/
+cp cl-daemon /usr/bin/
 mkdir /etc/change_log
-mv config.json /etc/change_log/
-mv change_log.service /etc/systemd/system/
+cp config.json /etc/change_log/
+cp change_log.service /etc/systemd/system/
 systemctl daemon-reload
 # start script on boot
 systemctl enable change_log.service
 mv cl-cli /usr/bin/
+set +x
